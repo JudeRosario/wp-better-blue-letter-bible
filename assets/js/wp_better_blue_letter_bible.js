b97f0b360bc493a9933bcb0ca6e7eeadb52ced38
Jude Rosario <judesrosario89@gmail.com>
2015-02-21 01:49:35 +0530

    Initial Build without src files

---
+++ assets/js/wp_better_blue_letter_bible.js
@@ -0,0 +1,8 @@
+/*! WP Better Blue Letter Bible - v0.1.0
+ * http://wordpress.org/plugins
+ * Copyright (c) 2015; * Licensed GPLv2+ */
+( function( window, undefined ) {
+	'use strict';
+
+
+} )( this );
